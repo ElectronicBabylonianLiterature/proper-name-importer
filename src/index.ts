dc946f609b1994e02e47e0b37e44dac3a44f57f3
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-06-25 15:13:48 +0200

    Load file and import words

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,63 @@
 import {Command, flags} from '@oclif/command'
+import cli from 'cli-ux'
+import {readFile} from 'fs'
+import {MongoClientOptions, MongoClient} from 'mongodb'
+import WordRepository from './word-repository'
+import ProperName from './proper-name'
+
+async function readJson(fileName: string): Promise<readonly ProperName[]> {
+  return new Promise<string>((resolve, reject) => {
+    readFile(fileName, 'utf8', (err, data) => err ? reject(err) : resolve(data))
+  })
+  .then(JSON.parse)
+  .then((json: any[]) => json.map(({lemma, pos, guideWord, origin}) => new ProperName(lemma, pos, guideWord, origin)))
+}
 
 class ProperNameImporter extends Command {
-  static description = 'describe the command here'
+  static description = 'Imports proper names from a JSON file to the Dictionary.'
 
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    host: flags.string({char: 'h', description: 'MongoDB URI', default: 'mongodb://localhost:27017'}),
+    db: flags.string({char: 'd', description: 'database name', default: 'ebl'}),
+    ssl: flags.boolean({description: 'Use SSL connection.'}),
   }
 
-  static args = [{name: 'file'}]
+  static args = [{name: 'file', description: 'Path to the JSON file', required: true}]
 
   async run() {
-    const {args, flags} = this.parse(ProperNameImporter)
+    const {args: {file}, flags: {host, db, ssl}} = this.parse(ProperNameImporter)
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from .\\src\\index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    try {
+      cli.action.start(`Loading guide words from ${file}...`)
+      const properNames = await readJson(file)
+      cli.action.stop()
+
+      cli.action.start(`Inserting words to MongoDB ${host}...`)
+      const options: MongoClientOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+      if (ssl) {
+        options.ssl = true
+        options.sslValidate = false
+      }
+      const client = new MongoClient(host, options)
+      const repository = new WordRepository(client, db)
+      try {
+        client.connect()
+        for (const properName of properNames) {
+          // eslint-disable-next-line no-await-in-loop
+          await repository.insertProperName(properName)
+        }
+      } finally {
+        client.close()
+      }
+
+      cli.action.stop()
+    } catch (error) {
+      this.error(error)
     }
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,47 @@
 import {expect, test} from '@oclif/test'
+import {MongoMemoryServer} from 'mongodb-memory-server'
+import {MongoClient} from 'mongodb'
 
 import cmd = require('../src')
 
 describe('proper-name-importer', () => {
   test
+  .add('db', () => 'ebltest')
+  .add('mongod', () => new MongoMemoryServer())
+  .finally(async ctx => ctx.mongod.stop())
+  .add('uri', async ctx => ctx.mongod.getUri())
+  .add('client', async ctx => new MongoClient(ctx.uri, {useNewUrlParser: true, useUnifiedTopology: true}))
+  .do(async ctx => ctx.client.connect())
+  .finally(async ctx => ctx.client.close())
+  .add('collection', ctx => ctx.client.db(ctx.db).collection('words'))
   .stdout()
-  .do(() => cmd.run([]))
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .do(() => cmd.run(['--name', 'jeff']))
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  .stderr()
+  .do(ctx => cmd.run(['--host', ctx.uri, '--db', ctx.db, './test/proper-names.json']))
+  .it('imports proper names', async ctx => {
+    const fragments = await ctx.collection.find().toArray()
+    expect(fragments).to.deep.equal([
+      {
+        _id: 'Abu I',
+        lemma: ['Abu'],
+        homonym: 'I',
+        attested: true,
+        legacyLemma: 'Abu I',
+        forms: [],
+        meaning: '',
+        logograms: [],
+        derived: [],
+        derivedFrom: null,
+        amplifiedMeanings: [],
+        pos: ['PN'],
+        guideWord: 'Abu (name)',
+        oraccWords: [
+          {
+            lemma: 'Abu',
+            guideWord: 'Abu (name)',
+          },
+        ],
+        origin: 'test',
+      },
+    ])
   })
 })
