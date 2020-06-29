0757341f5a13e48bb3258880367612df20555393
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-06-29 10:10:50 +0200

    Refactor WordRepository test

--- test/word-repository.test.ts
+++ test/word-repository.test.ts
@@ -1,10 +1,10 @@
 import {expect} from '@oclif/test'
 import {withDatabase} from './context'
 import WordRepository from '../src/word-repository'
-import ProperName from './../src/proper-name'
+import ProperName from '../src/proper-name'
 
 describe('WordRepository', () => {
-  withDatabase
+  const insertWord = withDatabase
   .add('repository', ctx => new WordRepository(ctx.client, ctx.db))
   .add('properName', () => new ProperName({
     lemma: 'Abu',
@@ -14,25 +14,22 @@ describe('WordRepository', () => {
     origin: 'test',
   }))
   .do(async ctx => ctx.repository.insertProperName(ctx.properName))
-  .it('creates words', async ctx => {
+
+  insertWord
+  .it('insertWord creates the word', async ctx => {
     const words = await ctx.collection.find().toArray()
     expect(words).to.deep.equal([
       ctx.properName.word,
     ])
   })
 
-  withDatabase
-  .add('repository', ctx => new WordRepository(ctx.client, ctx.db))
-  .add('properName', () => new ProperName({
-    lemma: 'Abu',
-    homonym: 1,
-    pos: 'PN',
-    guideWord: 'Abu (name)',
-    origin: 'test',
-  }))
-  .do(async ctx => ctx.repository.insertProperName(ctx.properName))
-  .it('hasWords', async ctx => {
+  insertWord
+  .it('hasWords returns true if the word exists', async ctx => {
     expect(await ctx.repository.hasWord('Abu I')).equal(true)
+  })
+
+  insertWord
+  .it('hasWords returns false if the word does not exist', async ctx => {
     expect(await ctx.repository.hasWord('Abu II')).equal(false)
   })
 })
